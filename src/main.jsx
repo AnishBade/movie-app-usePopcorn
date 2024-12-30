937936a9283684cddbb57fc35e770ba609a43904
Anish Bade <badeanish6045@gmail.com>
2024-12-30 20:23:05 +0545

    reusable star component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { useState } from "react";
 // import './index.css'
 // import App from './App.jsx'
 
 import StarRating from "./StarRating";
+
+
+function Test(){
+  const [movieRating, setMovieRating] = useState(0);
+  console.log(movieRating)
+  return <StarRating color="blue" onSetRating={setMovieRating}/>
+
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -12,6 +21,7 @@ createRoot(document.getElementById("root")).render(
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
-    <StarRating size={24} color="red" />
+    <StarRating size={24} color="red" defaultRating={3} />
+    <Test/>
   </StrictMode>
 );
