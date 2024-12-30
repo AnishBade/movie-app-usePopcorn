31791cd0ae1285d21d33d8abd802a1098f7c859e
Anish Bade <badeanish6045@gmail.com>
2024-12-30 06:55:41 +0545

    passing element as prop

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,15 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
+        {/* <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched} />
+            y</>
+          }
+        /> */}
         <Box>
           <MovieList movies={movies} />
         </Box>
