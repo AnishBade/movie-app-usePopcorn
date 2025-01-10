bffc1b2859981e4755e2c362c3d0a763af0f20de
Anish Bade <badeanish6045@gmail.com>
2025-01-10 22:57:43 +0545

    ref to select dom

--- src/App.jsx
+++ src/App.jsx
@@ -176,8 +176,20 @@ function Search({ query, setQuery }) {
   const inputEl = useRef(null);
 
   useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+    function callback(e) {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+      inputEl.current.focus();
+    }
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
+  }, [setQuery]);
 
   return (
     <input
